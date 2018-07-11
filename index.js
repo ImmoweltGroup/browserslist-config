d86ee6c485ac1223f4893dc297a352d38187424c
Kevin Ruff <kevin.ruff@immonet.de>
2018-07-11 15:09:09 +0200

    FIX: Add editorconfig.

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 module.exports = [
-    'android >= 4',
-    'last 2 Baidu versions',
-    // BlackBerry not supported
-    'last 10 Chrome versions',
-    'last 2 ChromeAndroid versions',
-    'last 2 Edge versions',
-    'last 2 Electron versions',
-    'ie >= 10',
-    'last 2 IE_mob versions',
-    'last 10 Firefox versions',
-    'Firefox ESR',
-    'last 2 FirefoxAndroid versions',
-    'ios >= 8',
-    'opera >= 12',
-    // OperaMini not supported
-    'last 2 OperaMobile versions',
-    'last 2 QQAndroid versions',
-    'safari >= 6',
-    'last 2 Samsung versions',
-    'last 2 UCAndroid versions'
+  'android >= 4',
+  'last 2 Baidu versions',
+  // BlackBerry not supported
+  'last 10 Chrome versions',
+  'last 2 ChromeAndroid versions',
+  'last 2 Edge versions',
+  'last 2 Electron versions',
+  'ie >= 10',
+  'last 2 IE_mob versions',
+  'last 10 Firefox versions',
+  'Firefox ESR',
+  'last 2 FirefoxAndroid versions',
+  'ios >= 8',
+  'opera >= 12',
+  // OperaMini not supported
+  'last 2 OperaMobile versions',
+  'last 2 QQAndroid versions',
+  'safari >= 6',
+  'last 2 Samsung versions',
+  'last 2 UCAndroid versions'
 ];
