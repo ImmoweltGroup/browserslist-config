b8635ce57d0caa7f071922b909c8b3c7b0d591b1
Dennis Kribl <dennis.kribl@immowelt.de>
2022-01-11 09:40:57 +0100

    PATCH: remove android native browser 4 support
    Signed-off-by: Dennis Kribl <dennis.kribl@immowelt.de>

--- src/index.js
+++ src/index.js
@@ -13,11 +13,9 @@ module.exports = [
   // Apple
   'last 3 Safari major versions',
   'last 3 iOS major versions',
-  // Android
-  'last 2 Android major versions',
   // Others
   'last 2 Electron major versions',
   'last 1 Samsung major versions',
 ];
 
-// Not supported: BlackBerry, OperaMini, Android, ExplorerMobile, Explorer, kaios, OperaMobile, Baidu
+// Not supported: BlackBerry, OperaMini, Android, ExplorerMobile, Explorer, kaios, OperaMobile, Baidu, Android Native
